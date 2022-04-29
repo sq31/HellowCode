352447e9aef8e9ad53ac2731b736905d3d530644
OstapchukSergey <sq31@mail.ru>
2022-04-29 18:37:56 +0300

    Random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 3;
-int numberB = 53;
+int numberA = new Random ().Next (1,10);
+Console.WriteLine(numberA);
+int numberB = new Random ().Next (1,10);
+Console.WriteLine(numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
 
