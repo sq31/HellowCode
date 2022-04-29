896a001d9d226abd693a884fe85edb4b6bd0b5df
OstapchukSergey <sq31@mail.ru>
2022-04-29 17:56:21 +0300

    Add Example 001 002 003

---
+++ Example003_sum/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = 3;
+int numberB = 53;
+int result = numberA+numberB;
+Console.WriteLine(result);
+
+
+
+
+//Console.WriteLine("Hello, World!");
